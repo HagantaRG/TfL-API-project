459fd58f89e46f5e51ed714a2f955e4b38843d09
HagantaRG <haganta.ginting@automation-consultants.com>
2025-03-16 13:57:46 +0000

    Added logging directory

---
+++ logs/__init__.py
@@ -0,0 +1,12 @@
+"""
+This directory contains the log files for the application.
+"""
+
+# Import from standard libraries
+from pathlib import Path
+from typing import Final
+
+
+# Define constants
+DIR: Final[Path] = Path(__file__).parent
+"""The path to this directory."""
