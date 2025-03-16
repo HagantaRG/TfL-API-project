9a25d06ce5f5ed0e407d4d5c59be24b845aa3266
HagantaRG <haganta.ginting@automation-consultants.com>
2025-03-16 14:01:46 +0000

    Added framework for context management of DB connection

--- source/python/db_client/__init__.py
+++ source/python/db_client/__init__.py
@@ -1,5 +1,13 @@
 # Import 3rd-party libraries
-import psycopg
+import logging
+import os
+from time import sleep
+from types import TracebackType
+from typing import Literal, Self, overload
+
+from psycopg import (connect, Connection)
+
+log = logging.getLogger(__name__)
 
 class DBClient:
     hostname: str
@@ -7,6 +15,7 @@ class DBClient:
     dbname: str
     user: str
     password: str
+    conn_str: str
 
     def __init__(
             self,
@@ -24,14 +33,104 @@ class DBClient:
 
     # Context managers
     def __enter__(self):
+        """
+        Enter the runtime context.
+        """
+
+        self.connection = self.connect()
+        self.connected = True if self.connection else False
+        return self
+
+    @overload
+    def __exit__(
+            self,
+            type_: type[BaseException],
+            exception: BaseException,
+            traceback: TracebackType,
+    ) -> Literal[False]:
         ...
-    def __exit__(self, exc_type, exc_val, exc_tb):
+
+    @overload
+    def __exit__(
+            self,
+            type_: None,
+            exception: None,
+            traceback: None,
+    ) -> Literal[False]:
         ...
+
+    def __exit__(
+            self,
+            type_: type[BaseException] | None = None,
+            exception: BaseException | None = None,
+            traceback: TracebackType | None = None,
+    ) -> Literal[False]:
+        """
+        Exit the runtime context with the exception logged.
+
+        :param type_: The type of exception encountered.
+        :param exception: The exception object itself.
+        :param traceback: The information on how the exception was reached.
+
+        :return: Whether the exception is suppressed. ``False``, since the
+            exception is only logged.
+        """
+
+        if exception is not None:
+            logging.exception(exception)
+        try:
+            self.connection.close()
+        except Exception as exception:
+            logging.exception(exception)
+            raise
+        self.connected = False
+        return False
+
+    @property
+    def connection_string(self):
+        return f"host={self.hostname} port={self.port} dnname={self.dbname} user={self.user} password={self.password}"
 
     # connect method to connect to DB
-    def connect(self):
-        ...
+    def connect(
+        self,
+        max_retry: int = 5,
+        wait_time: float = 0.5,
+        retry_exponent: float = 1.1
+    ) -> Connection: # I'm not sure why this says this *can* return a None.
+        for retries in range(max_retry + 1):
+            try:
+                connection: Connection = connect(conninfo=self.connection_string)
+                log.info(f"Connection to {self.dbname} successful.")
+                return connection
+            except Exception as error:
+                message: str = f"Could not connect to DB, attempting to " \
+                           f"reconnect.\n{error}"
+                logging.error(message)
+                print(message)
+                while not self._check_connection():
+                    message: str = "Unable to connect to database for " \
+                                   "network reasons, please check your connection/VPN."
+                    print(message)
+                    sleep(2)
+                if retries == max_retry:
+                    message: str = f"Max retries reached, raising error.\n{error}"
+                    logging.error(message)
+                    print(message)
+                    raise ConnectionError from error
+                else:
+                    sleep(wait_time * (retry_exponent ** retries))
 
-    def select_query(self):
-        ...
+    # NB just have one method that executes *ANY* kind of query. I don't want to think about this further for now.
 
+    # Private methods
+    def _check_connection(self) -> bool:
+        """
+        Check the connection to the database.
+
+        :return: Whether the database is reachable.
+        """
+
+        command: str = f"sqlcmd -S {self.hostname} -d {self.dbname} " \
+                       f"-U {self.user} -P {self.password} -q EXIT"
+        result: int = os.system(command)
+        return result == 0
