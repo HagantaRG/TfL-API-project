1c02dcea7e16330cfcad14f399d380f225ec868b
haganta-g <50399914+haganta-g@users.noreply.github.com>
2026-02-18 23:20:00 +0700

    Modified datatypes, added dataclass decorators.

--- source/python/datatypes/__init__.py
+++ source/python/datatypes/__init__.py
@@ -1,18 +1,25 @@
 from __future__ import annotations
+from dataclasses import dataclass
 
+@dataclass
 class Train:
     destination_stop: Stop
     previous_stop: Stop
     next_stop: Stop
     ...
 
+@dataclass
 class Stop:
     adjacent_stops: list[Stop]
     lat: float
     long: float
     lines: list[Line]
+    stop_name: str
+    naptan_id: str
     ...
 
+@dataclass
 class Line:
     stops: list[Stop]
+    line_name: str
     ...
