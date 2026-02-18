bd8a077e851eb1106cec4114d7c48ee25fcec18a
haganta-g <50399914+haganta-g@users.noreply.github.com>
2026-02-18 18:29:26 +0700

    Basic datatypes for tracking train motion.

--- source/python/datatypes/__init__.py
+++ source/python/datatypes/__init__.py
@@ -1,14 +1,18 @@
 from __future__ import annotations
 
 class Train:
+    destination_stop: Stop
+    previous_stop: Stop
+    next_stop: Stop
     ...
 
 class Stop:
     adjacent_stops: list[Stop]
     lat: float
     long: float
+    lines: list[Line]
     ...
 
 class Line:
-
+    stops: list[Stop]
     ...
